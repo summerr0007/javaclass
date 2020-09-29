8d1be26a2565bb4ab407cb6cea77759890805291
summerr0007 <65068498+summerr0007@users.noreply.github.com>
2020-09-29 20:55:51 +0800

    Add files via upload

---
+++ Q2_6.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Q2_6 {
+	public static void main() {
+		Scanner sca = new Scanner(System.in);
+		int num = sca.nextInt();
+		ArrayList<String> str = new ArrayList<>();
+		StringBuffer strb = new StringBuffer();
+		for(int i = 0;i < num;i++){
+			str.add(sca.next());
+		}
+		for(int i = 0;i < num;i++){
+			for (int j = 0;j<str.get(i).length()-1;j++){
+				 
+			}
+		}
+	}
+}
