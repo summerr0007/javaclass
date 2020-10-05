bea00a304aa2c2b100625c280eb1b7a5fe355e82
summerr0007 <65068498+summerr0007@users.noreply.github.com>
2020-10-05 23:30:04 +0800

    Add files via upload

--- Q2_6.java
+++ Q2_6.java
@@ -1,18 +1,22 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.text.DecimalFormat;
 public class Q2_6 {
-	public static void main() {
-		Scanner sca = new Scanner(System.in);
-		int num = sca.nextInt();
-		ArrayList<String> str = new ArrayList<>();
-		StringBuffer strb = new StringBuffer();
-		for(int i = 0;i < num;i++){
-			str.add(sca.next());
-		}
-		for(int i = 0;i < num;i++){
-			for (int j = 0;j<str.get(i).length()-1;j++){
-				 
-			}
-		}
-	}
+    public static void main(String arg[]){
+        int num;
+        ArrayList<String> arrayList = new ArrayList();
+        Scanner scanner = new Scanner(System.in);
+        num = scanner.nextInt();
+        for(int i = 0; i < num; i++){
+            arrayList.add(scanner.next());
+        }
+        for(int i = 0; i < num; i++){
+            String t = arrayList.get(i);
+            for(int j = 0 ;j < t.length()-1;j++){
+                System.out.print(Math.abs(t.charAt(j) - t.charAt(j+1)));
+            }
+            System.out.print("\n");
+        }
+    }
 }
